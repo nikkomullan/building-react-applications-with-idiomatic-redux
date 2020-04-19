e08d301ae11b438de9feeff9b4656d1801ca4304
Nicolas Martinez <nicolas.martinez@klarna.com>
2020-04-19 18:29:21 +0200

    Add initial persisted state

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
-import 'babel-polyfill';
-import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import todoApp from './reducers';
-import App from './components/App';
+import 'babel-polyfill'
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import App from './components/App'
 
-const store = createStore(todoApp);
+const persistedState = {
+  todos: [
+    {
+      id: 0,
+      text: 'Welcome back',
+      completed: false,
+    },
+  ],
+}
+
+const store = createStore(todoApp, persistedState)
 
 render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
-);
+)
