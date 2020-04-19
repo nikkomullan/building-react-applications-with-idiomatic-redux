eed1157013440b2ffd679f3fbe7ccc14205d28dc
Nicolas Martinez <nicolas.martinez@klarna.com>
2020-04-19 18:21:56 +0200

    Simplify the arrow functions

--- src/components/FilterLink.js
+++ src/components/FilterLink.js
@@ -1,24 +1,19 @@
-import { connect } from 'react-redux';
-import { setVisibilityFilter } from '../actions';
-import Link from './Link';
+import { connect } from 'react-redux'
+import { setVisibilityFilter } from '../actions'
+import Link from './Link'
 
 const mapStateToProps = (state, ownProps) => {
   return {
     active: ownProps.filter === state.visibilityFilter,
-  };
-};
+  }
+}
 
-const mapDispatchToProps = (dispatch, ownProps) => {
-  return {
-    onClick: () => {
-      dispatch(setVisibilityFilter(ownProps.filter));
-    },
-  };
-};
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  onClick() {
+    dispatch(setVisibilityFilter(ownProps.filter))
+  },
+})
 
-const FilterLink = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Link);
+const FilterLink = connect(mapStateToProps, mapDispatchToProps)(Link)
 
-export default FilterLink;
+export default FilterLink
