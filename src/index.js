57992cb5d7cdcde300e9676189bb0819e9511bc8
Nicolas Martinez <nicolas.martinez@klarna.com>
2020-04-19 18:44:31 +0200

    Add throttle for state persistance

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import throttle from 'lodash/throttle'
 import todoApp from './reducers'
 import App from './components/App'
 import { loadState, saveState } from './localStorage'
@@ -11,11 +12,13 @@ const persistedState = loadState()
 
 const store = createStore(todoApp, persistedState)
 
-store.subscribe(() => {
-  saveState({
-    todos: store.getState().todos,
-  })
-})
+store.subscribe(
+  throttle(() => {
+    saveState({
+      todos: store.getState().todos,
+    })
+  }, 1000)
+)
 
 render(
   <Provider store={store}>
