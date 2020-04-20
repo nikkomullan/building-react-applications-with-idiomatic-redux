c84ac771eacd246f5e9779dacbff767cfd7514df
Nicolas Martinez <nicolas.martinez@klarna.com>
2020-04-20 20:23:19 +0200

    Add getVisibleTodos selector

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -5,34 +5,42 @@ const todo = (state, action) => {
         id: action.id,
         text: action.text,
         completed: false,
-      };
+      }
     case 'TOGGLE_TODO':
       if (state.id !== action.id) {
-        return state;
+        return state
       }
       return {
         ...state,
         completed: !state.completed,
-      };
+      }
     default:
-      return state;
+      return state
   }
-};
+}
 
 const todos = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
-      return [
-        ...state,
-        todo(undefined, action),
-      ];
+      return [...state, todo(undefined, action)]
     case 'TOGGLE_TODO':
-      return state.map(t =>
-        todo(t, action)
-      );
+      return state.map((t) => todo(t, action))
     default:
-      return state;
+      return state
   }
-};
+}
 
-export default todos;
+export default todos
+
+export const getVisibleTodos = (state, filter) => {
+  switch (filter) {
+    case 'all':
+      return state
+    case 'completed':
+      return state.filter((t) => t.completed)
+    case 'active':
+      return state.filter((t) => !t.completed)
+    default:
+      throw new Error(`Unknown filter: ${filter}.`)
+  }
+}
