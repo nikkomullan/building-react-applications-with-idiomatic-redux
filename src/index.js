1c3c2a2ed02f5faa24e7c202e143d4e9966d3519
Nicolas Martinez <nicolas.martinez@klarna.com>
2020-04-21 21:14:41 +0200

    Add fake backend API

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import React from 'react'
 import { render } from 'react-dom'
 import Root from './components/Root'
 import configureStore from './configureStore'
+import { fetchTodos } from './api'
+
+fetchTodos('all').then(console.log)
 
 const store = configureStore()
 
