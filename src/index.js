41ac3722105cc4572d957d8f91785de9af82ffca
Nicolas Martinez <nicolas.martinez@klarna.com>
2020-04-21 21:44:17 +0200

    Remove fetchTodos logic from main rendering module

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import React from 'react'
 import { render } from 'react-dom'
 import Root from './components/Root'
 import configureStore from './configureStore'
-import { fetchTodos } from './api'
-
-fetchTodos('all').then(console.log)
 
 const store = configureStore()
 
