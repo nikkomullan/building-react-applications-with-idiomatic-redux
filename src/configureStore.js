7677f482b99a7de81a9edab6c9e1369ed929cba5
Nicolas Martinez <nicolas.martinez@klarna.com>
2020-04-26 20:10:36 +0200

    Use applyMiddleware

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,28 +1,16 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import promise from 'redux-promise'
 import logger from 'redux-logger'
 import todoApp from './reducers'
 
-const wrapDispatchWithMiddlewares = (store, middlewares) => {
-  middlewares
-    .slice()
-    .reverse()
-    .forEach(middleware => {
-      store.dispatch = middleware(store)(store.dispatch)
-    })
-}
-
 const configureStore = () => {
-  const store = createStore(todoApp)
   const middlewares = [promise]
 
   if (process.env.NODE_ENV !== 'production') {
     middlewares.push(logger)
   }
 
-  wrapDispatchWithMiddlewares(store, middlewares)
-
-  return store
+  return createStore(todoApp, applyMiddleware(...middlewares))
 }
 
 export default configureStore
